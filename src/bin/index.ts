5edcfcf49b9b7c72c369a4312297de9d3cb1fd0b
wgfire <364049242@qq.com>
2022-07-17 22:15:58 +0800

    perf(bin): 优化bin的代码结构

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -5,17 +5,29 @@
 *@reviewContent wg
 1.尝试用babel 来解析ts 解决路径别名编译后没有替换的问题
 */
-import { Command } from 'commander';
-import { getEslint } from '@/eslint/index';
+import { Command, CommandOptions } from "commander";
+import { getEslint } from "@/eslint/index";
 
 const program = new Command();
 
-program
-  .version('0.1.0')
-  .description('start eslint and fix code')
-  .command('eslint')
-  .action((value) => {
-    console.log(value, '参数');
+program.version("0.1.0").description("cli-boy eslint webpack add Project");
+
+const eslint = program
+  .command("eslint")
+  .description("start eslint code")
+  .option("-base", "base eslint parser") // 定义options 范围
+  .option("-airbnb", "airbnb eslint parser")
+  .action((optios, arg) => {
+    console.log(optios, arg.args, "参数"); // 第一个参数显示 {Base:true,Airbnb:true} 用了哪个参数
     getEslint();
   });
+const webpack = program
+  .command("webpack")
+  .description("start webpack project")
+  .action((value) => {
+    console.log(value, "参数");
+    getEslint();
+  });
+
+program.commands = [eslint, webpack];
 program.parse(process.argv);
